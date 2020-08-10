f9f67baaea2bf4898040cc93158726a89348be03
Jingjuan-Mac <690329169@qq.com>
2020-08-10 21:22:56 +0800

    add class IMuduoUser,库和用户分类

---
+++ IMuduoUser.h
@@ -0,0 +1,20 @@
+//
+// Created by Jingjuan on 2020/8/10.
+//
+
+#ifndef MINI_MUDUO_IMUDUOUSER_H
+#define MINI_MUDUO_IMUDUOUSER_H
+
+#include <string>
+//#include "TcpConnection.h"
+using namespace std;
+
+class TcpConnection;
+
+class IMuduoUser{
+public:
+    void virtual onConnection(TcpConnection* pCon) = 0;
+    void virtual onMessage(TcpConnection* pCon, const string& data) = 0;
+};
+
+#endif //MINI_MUDUO_IMUDUOUSER_H
